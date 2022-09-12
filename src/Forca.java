a00927c23b8383b0413f4ea44639d6401b5dad82
tfxsoftware <aswespeak.tfx@gmail.com>
2022-09-12 14:57:04 -0300

    metodos distintos completos

--- src/Forca.java
+++ src/Forca.java
@@ -1,4 +1,4 @@
-import java.io.*;
+
 
 public class Forca
 {
--- src/Tracinhos.java
+++ src/Tracinhos.java
@@ -1,13 +1,18 @@
+
+
 public class Tracinhos implements Cloneable
 {
-    private char texto [];
+    private char[] texto ;
 
     public Tracinhos (int qtd) throws Exception
     {
 		if (qtd <= 0){
             throw new Exception("Quantidade de tracinhos inválida!");
         }
-        this.texto = 
+        this.texto = new char[qtd];
+        for (int i=0;i<qtd;i++){
+            texto[i] = '_';
+        }
         
         // verifica se qtd não é positiva, lançando uma exceção.
 		// instancia this.texto com um vetor com tamanho igual qtd.
@@ -16,7 +21,11 @@ public class Tracinhos implements Cloneable
 
     public void revele (int posicao, char letra) throws Exception
     {
-		// verifica se posicao é negativa ou então igual ou maior
+		if (posicao < 0 || posicao >= this.texto.length){
+            throw new Exception("Posição inválida!");
+        }
+        this.texto[posicao] = letra;
+        // verifica se posicao é negativa ou então igual ou maior
 		// do que this.texto.length, lançando uma exceção.
 		// armazena a letra fornecida na posicao tambem fornecida
 		// do vetor this.texto
@@ -24,6 +33,15 @@ public class Tracinhos implements Cloneable
 
     public boolean isAindaComTracinhos ()
     {
+        for (int i=0;i<this.texto.length;i++){
+            if (this.texto[i] == '_'){
+                return true;
+            }
+        
+        }
+        return false;
+        
+        
         // percorre o vetor de char this.texto e verifica
         // se o mesmo ainda contem algum underline ou se ja
         // foram todos substituidos por letras; retornar true
@@ -33,6 +51,11 @@ public class Tracinhos implements Cloneable
 
     public String toString ()
     {
+        String caracteres = "";
+        for (int i=0;i<this.texto.length;i++){
+            caracteres = caracteres + this.texto[i] + " ";
+        }
+        return caracteres;
         // retorna um String com TODOS os caracteres que há
         // no vetor this.texto, intercalados com espaços em
         // branco
